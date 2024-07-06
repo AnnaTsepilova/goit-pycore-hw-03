a8faaa8d1d486617deb442f4f8d0ac91f6c75171
Anna Tsepilova <virgo.tsav@gmail.com>
2024-07-06 17:46:47 +0300

    Revert to old version

--- third_task.py
+++ third_task.py
@@ -4,13 +4,10 @@ def normalize_phone(phone_number: str):
     pattern = r"[^+\d]"
     number = re.sub(pattern, '', phone_number)
 
-    # match = re.search(r"(\+)?(38)?(\d+)", number)
-    # number = match.group(1) if match.group(1) else "+"
-    # number += match.group(2) if match.group(2) else "38"
-    # number += match.group(3)
-    ## print(match.group(1), match.group(2), match.group(3))
-
-    number = re.sub(r"(\+)?(38)?(\d{6})", r"+38\3", number)
+    match = re.search(r"(\+)?(38)?(\d+)", number)
+    number = match.group(1) if match.group(1) else "+"
+    number += match.group(2) if match.group(2) else "38"
+    number += match.group(3)
 
     return number
 
